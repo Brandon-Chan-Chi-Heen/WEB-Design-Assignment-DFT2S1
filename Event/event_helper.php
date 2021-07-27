617176a5288ae86061a52b058b9ed263ef975baf
Brandon-Chan-Chi-Heen <79690019+Brandon-Chan-Chi-Heen@users.noreply.github.com>
2021-07-27 12:25:52 +0800

    Add files via upload

---
+++ Event/event_helper.php
@@ -0,0 +1,50 @@
+<?php
+    //Extract From Database
+    function getEventDetails(){
+        define('DB_HOST', 'localhost');
+        define('DB_USER', 'root');
+        define('DB_PASS', '');
+        define('DB_NAME', 'eventlist');
+        $con = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+        $sql = "SELECT * FROM display_event";
+        if ($result = $con->query($sql)){
+            $pictureNumber = 1;
+            while ($row = $result->fetch_object())
+            {
+                //Event Details
+                $event_List = <<< HELLO
+                <div class="Event">
+                    <div  class="col-1-3 specials">
+                    <img src="eventNo$pictureNumber.jpg" alt="" />
+                    </div>
+                    <div class="col-2-3 specials">
+                        <div class="Details">
+                            <div class="uploadEdit">
+                                <h3><a href="" class="bookmark">🔖</a></h3>
+                            </div>
+                            <h1>$row->Event_Title</h1>
+                            <p>$row->Event_Description</p>
+                        </div>
+                        <div class="price">
+                            <h3><a href="" class="addToCart">🛒</a>$$row->Event_Price</h3>
+                            <a href="" class="enrollNow"><h3>Enroll Now</h3></a>
+                        </div>
+                    </div>
+HELLO;
+                echo $event_List;
+                $pictureNumber++;
+            }
+        }
+        
+    }
+
+
+
+
+
+
+
+
+
+    
+?>
